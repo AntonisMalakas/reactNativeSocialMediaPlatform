18463a1930a11e09748e20b6fdbfd2db0643f452
DonAnton <anthony.elhaddad@outlook.com>
2020-04-10 20:26:49 +0300

    added icons to tab navigator

---
+++ src/components/tabBarComponent.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+    Text,
+    StatusBar,
+    View,
+    StyleSheet,
+    TouchableOpacity
+} from 'react-native';
+import Icon from 'react-native-vector-icons/Entypo';
+import { DefaultText } from '../baseComponent/defaultText';
+import { constStyle } from '../baseComponent/constStyle';
+
+
+
+export function TabBarComponent({ state, descriptors, navigation }) {
+    return (
+        <View style={[styles.tabContainer, constStyle.shadow.depth4]}>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Home')} style={[styles.tabItem]}>
+                <Icon name="home" size={25} color={state.index == 0 ? constStyle.baseColor : 'darkgrey'} />
+                <DefaultText text="Home" level={0} state={state.index == 0 ? 'active' : 'deactive'} />
+            </TouchableOpacity>
+
+            {/* <TouchableOpacity style={[styles.tabItem]} onPress={() => navigation.navigate('NewChat')}>
+                <View style={{ backgroundColor: constStyle.baseColor, width: 60, height: 60, borderRadius: 60 / 2, justifyContent: 'center', alignItems: 'center', bottom: 10 }}>
+                    <Icon name="plus" size={30} color='white' />
+                </View>
+            </TouchableOpacity> */}
+
+            <TouchableOpacity onPress={() => navigation.navigate('Settings')} style={[styles.tabItem]}>
+                <Icon name="tools" size={25} color={state.index == 1 ? constStyle.baseColor : 'darkgrey'} />
+                <DefaultText text="Settings" level={0} state={state.index == 1 ? 'active' : 'deactive'} />
+            </TouchableOpacity>
+
+        </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    tabContainer: {
+        backgroundColor: 'white',
+        height: 50,
+        flexDirection: 'row'
+    },
+    tabItem: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+});
--- src/navigation/HomeNavigator.js
+++ src/navigation/HomeNavigator.js
@@ -6,14 +6,17 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/home/HomeScreen';
 import SettingsScreen from '../screens/settings/SettingsScreen';
 
+import { Ionicons } from 'react-native-vector-icons/Ionicons';
+
+import { TabBarComponent } from '../components/tabBarComponent';
 
 const Tab = createBottomTabNavigator();
 
 
 class HomeNavigator extends Component {
     render() {
-        return (
-            <Tab.Navigator>
+        return ( 
+            <Tab.Navigator tabBar={props => <TabBarComponent {...props} />}>
                 <Tab.Screen name="Home" component={HomeScreen} />
                 <Tab.Screen name="Settings" component={SettingsScreen} />
             </Tab.Navigator>
